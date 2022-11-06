3e78bbbe8fbe3557061874a95b977bdce2a929dd
Thomas-Herner1 <tuj49289@temple.edu>
2022-11-06 15:16:38 -0500

    -Test collection of accelerometer data

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    SensorManager manageSensors;
+    Sensor accelerometerSensor;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manageSensors = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = manageSensors.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        manageSensors.registerListener(MainActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorChanged) {
+        float[] tempHolder = sensorChanged.values;
+        if(tempHolder[0] > 0){
+            System.out.println("Change of: " + tempHolder[0] + " detected");
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
